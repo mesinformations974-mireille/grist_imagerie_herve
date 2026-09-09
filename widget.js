f0bc2cc3b24aa6b0dee28af1bb288830e442d441
TheBlueMimi <mesinformations974@gmail.com>
2026-09-09 09:40:39 +0200

    Update widget.jscomplement titre anglais

--- widget.js
+++ widget.js
@@ -348,8 +348,8 @@ var i18n = {
     templateUpdated: 'Modèle mis à jour !'
   },
   en: {
-    appTitle: 'VetAgroSup SIRIUS tasks Tracking',
-    appSubtitle: 'Organize and track tasks for SIRIUS projects',
+    appTitle: 'IMAGING MANAGEMENT – VETAGROSUP',
+    appSubtitle: "Organiser et suivre les rendez-vous et suivis d'imagerie à VetagroSup",
     notInGrist: 'This widget must be used inside Grist.',
     tabCalendar: 'Calendar',
     tabKanban: 'Kanban',
